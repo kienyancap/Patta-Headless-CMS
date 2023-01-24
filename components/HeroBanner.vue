6a9906003da48b04b726e8436544beb1ce2711e0
Kien Yan <kienyanneko@gmail.com>
2023-01-24 20:53:00 +0100

    make show products btn dynamic

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="hero-banner" :style="{ background: `${data.bg_color}` }">
     <div :class="[title === 'about-content' ? title : 'home-content']">
-      <h1 class="hero-title" :style="{ color: data.text_color ? data.text_color : '#000' }">
+      <h1
+        class="hero-title"
+        :style="{ color: data.text_color ? data.text_color : '#000' }"
+      >
         {{ data.banner_title }}
       </h1>
       <p
         :class="[
           'hero-description',
-          title === 'about-content' ? 'about-desc' : ''
+          title === 'about-content' ? 'about-desc' : '',
         ]"
         :style="{ color: data.text_color ? data.text_color : '#737b7d' }"
       >
         {{ data.banner_description }}
       </p>
-      <template>
-        <router-link
-          v-if="data.call_to_action.title && data.call_to_action.href"
+
+      <template v-if="[title === 'product-content' ? title : 'home-content']">
+        <NuxtLink
           aria-current="page"
           class="btn tertiary-btn"
-          to="/"
+          :to="data.call_to_action.href"
+          >{{ data.call_to_action.title }}</NuxtLink
         >
-          {{ data.call_to_action.title }}
-        </router-link>
       </template>
     </div>
     <img
@@ -33,21 +35,19 @@
 </template>
 
 <script lang="ts">
-
 import { PropType } from 'vue'
 import Data from '../typescript/data'
 
 export default {
-
   props: {
     data: {
       required: true,
-      type: Object as PropType<Data>
+      type: Object as PropType<Data>,
     },
     title: {
       required: true,
-      type: String
-    }
-  }
+      type: String,
+    },
+  },
 }
 </script>
